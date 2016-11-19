747cdd24164cd46f462fbb300d64084979ab985a
alriefqy <ekydesmito@gmail.com>
2016-11-19 18:17:28 +0800

    update halaman admin

---
+++ application/views/beranda.php
@@ -0,0 +1,29 @@
+<?php
+echo '
+	
+	<nav>
+		<ul>
+			<li><a href="#">Beranda</a></li>
+			<li><a href="#">News</a></li>
+			<li><a href="#">Penelitian</a></li>
+			<li><a href="#">Kemahasiswaan</a></li>
+			<li><a href="#">Fasilitas</a></li>
+		</ul>
+	</nav>
+';
+echo '<hr>';
+
+echo '<marquee>Welcome Home Bitch</marquee>';
+
+
+
+?>
+<style type="text/css">
+	nav ul li
+	{
+		display:inline;
+		padding:10px;
+	}
+
+</style>
+
